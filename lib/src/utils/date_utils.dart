f584fae459254bc1fc34a809c953ccf4904f4c33
Alex Chiew <concussion239@gmail.com>
2024-06-13 00:06:23 +0800

    Add computeAge method in DateUtils to calculate age from date of birth

---
+++ lib/src/utils/date_utils.dart
@@ -0,0 +1,11 @@
+class DateUtils {
+  static int computeAge(DateTime dateOfBirth) {
+    DateTime today = DateTime.now();
+    int age = today.year - dateOfBirth.year;
+    if (today.month < dateOfBirth.month ||
+        (today.month == dateOfBirth.month && today.day < dateOfBirth.day)) {
+      age--;
+    }
+    return age;
+  }
+}
